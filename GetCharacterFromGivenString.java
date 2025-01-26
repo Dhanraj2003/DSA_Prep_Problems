f9ba3845eab23f06330a038ce5254c1c6d2b4b50
Dhanraj Pawar <123626150+Dhanraj2003@users.noreply.github.com>
2025-01-26 23:50:13 +0530

    string_based_problems

---
+++ GetCharacterFromGivenString.java
@@ -0,0 +1,19 @@
+package string_based_programs;
+
+public class GetCharacterFromGivenString {
+
+    public static void main(String[] args) {
+
+
+        String str="vaibhav";
+
+        int index = 5;
+
+        char ch = str.charAt(index);
+
+        System.out.println("Character from " + str + " at index " + index + " is " + ch);
+    }
+
+
+    }
+
